0a24a29731d7d400390c35bae56b41810174a92b
Dardallin <dardallin@gmail.com>
2019-08-29 15:07:03 +0500

    Applied a jQuery for shortcutting code for <li> elements.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 $("li").click(function () {
-    if($(this).css("color") === "rgb(128, 128, 128)") {
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        })
-    } else {
-        $(this).css({
-            color: "grey",
-            textDecoration: "line-through"
-        })
-    }
+    // if($(this).css("color") === "rgb(128, 128, 128)") {
+    //     $(this).css({
+    //         color: "black",
+    //         textDecoration: "none"
+    //     })
+    // } else {
+    //     $(this).css({
+    //         color: "grey",
+    //         textDecoration: "line-through"
+    //     })
+    // }
+    $(this).toggleClass("done");
+
 });
